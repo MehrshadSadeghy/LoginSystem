87185779649e6473758a8c4b68bac5d67759bfab
mehrshad <mehrshad83sadeghi@gmail.com>
2024-01-04 12:48:59 +0330

    for push on github 2

--- LoginRegister/views.py
+++ LoginRegister/views.py
@@ -1,1 +1,25 @@
-it
+from django.shortcuts import render
+
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import UserSerializer
+
+
+class ShowAllUsersView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
